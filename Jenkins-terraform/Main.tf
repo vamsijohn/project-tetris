79fdbbf20f72bf372afe9d09d883df5ab733144a
Chandanasai1712 <chandanasai.rpr@gmail.com>
2024-03-17 23:03:05 +0530

    commit

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,9 +59,9 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
-  instance_type          = "t2.medium"
-  key_name               = "purplehaze"
+  ami                    = "ami-0cd59ecaf368e5ccf"
+  instance_type          = "t2.large"
+  key_name               = "tetris-v1"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
