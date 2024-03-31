8b5607ae789f053b9de165306d382f7539491167
vamsijohn <161419783+vamsijohn@users.noreply.github.com>
2024-03-31 15:59:19 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tetris-v17" # Replace with your actual S3 bucket name
+    bucket = "tetris91" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
